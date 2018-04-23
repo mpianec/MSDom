140c5060878d8d7b0d7311d3d00ccd5289334f29
mpianec <mpianec@foi.hr>
2018-04-23 22:07:35 +0200

    Dodan gumb.

--- MSDom/MSDom/Form1.Designer.cs
+++ MSDom/MSDom/Form1.Designer.cs
@@ -28,12 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Gumb = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Gumb
+            // 
+            this.Gumb.Location = new System.Drawing.Point(0, 0);
+            this.Gumb.Name = "Gumb";
+            this.Gumb.Size = new System.Drawing.Size(75, 23);
+            this.Gumb.TabIndex = 0;
+            this.Gumb.Text = "button1";
+            this.Gumb.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.Gumb);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button Gumb;
     }
 }
 
