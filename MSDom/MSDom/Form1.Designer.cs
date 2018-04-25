259dc2d717e2ada2a996e98d0ad6ef83af83b899
kristijan-perkovic96 <kperkovic@foi.hr>
2018-04-25 20:48:20 +0200

    Promjenjena pozadina.

--- MSDom/MSDom/Form1.Designer.cs
+++ MSDom/MSDom/Form1.Designer.cs
@@ -44,6 +44,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(284, 261);
             this.Controls.Add(this.Gumb);
             this.Name = "Form1";
