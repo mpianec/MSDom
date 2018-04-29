d2403a926d4a19200a9ddf0e48b1f5c6ea1915e2
kristijan-perkovic96 <kperkovic@foi.hr>
2018-04-29 22:10:25 +0200

    Napavljen cijeli dizajn forme Prijava.

--- MSDom/MSDom/Form1.Designer.cs
+++ MSDom/MSDom/Form1.Designer.cs
@@ -28,34 +28,174 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.Gumb = new System.Windows.Forms.Button();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.uiActionPrijaviSe = new System.Windows.Forms.Button();
+            this.uiInputLozinka = new System.Windows.Forms.TextBox();
+            this.uiInputKorisnickoIme = new System.Windows.Forms.TextBox();
+            this.uiClickExit = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // Gumb
+            // panel1
             // 
-            this.Gumb.Location = new System.Drawing.Point(0, 0);
-            this.Gumb.Name = "Gumb";
-            this.Gumb.Size = new System.Drawing.Size(75, 23);
-            this.Gumb.TabIndex = 0;
-            this.Gumb.Text = "button1";
-            this.Gumb.UseVisualStyleBackColor = true;
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(281, 424);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(74)))), ((int)(((byte)(184)))), ((int)(((byte)(161)))));
+            this.panel2.Controls.Add(this.uiClickExit);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.uiActionPrijaviSe);
+            this.panel2.Controls.Add(this.uiInputLozinka);
+            this.panel2.Controls.Add(this.uiInputKorisnickoIme);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(281, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(281, 424);
+            this.panel2.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(48, 47);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(179, 171);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Gabriola", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(41, 242);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(188, 39);
+            this.label1.TabIndex = 1;
+            this.label1.Text = " MSDom Vaš starački dom";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Gabriola", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(55)))), ((int)(((byte)(64)))));
+            this.label4.Location = new System.Drawing.Point(39, 209);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 35);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Lozinka";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Gabriola", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(55)))), ((int)(((byte)(64)))));
+            this.label3.Location = new System.Drawing.Point(39, 133);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(104, 35);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Korisničko ime";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Gabriola", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(99, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 50);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Prijava";
+            // 
+            // uiActionPrijaviSe
+            // 
+            this.uiActionPrijaviSe.BackColor = System.Drawing.Color.White;
+            this.uiActionPrijaviSe.Font = new System.Drawing.Font("Gabriola", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.uiActionPrijaviSe.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(55)))), ((int)(((byte)(64)))));
+            this.uiActionPrijaviSe.Location = new System.Drawing.Point(42, 330);
+            this.uiActionPrijaviSe.Name = "uiActionPrijaviSe";
+            this.uiActionPrijaviSe.Size = new System.Drawing.Size(200, 39);
+            this.uiActionPrijaviSe.TabIndex = 8;
+            this.uiActionPrijaviSe.Text = "Prijavi se";
+            this.uiActionPrijaviSe.UseVisualStyleBackColor = false;
+            // 
+            // uiInputLozinka
+            // 
+            this.uiInputLozinka.Location = new System.Drawing.Point(42, 244);
+            this.uiInputLozinka.Multiline = true;
+            this.uiInputLozinka.Name = "uiInputLozinka";
+            this.uiInputLozinka.Size = new System.Drawing.Size(200, 30);
+            this.uiInputLozinka.TabIndex = 7;
+            // 
+            // uiInputKorisnickoIme
+            // 
+            this.uiInputKorisnickoIme.Location = new System.Drawing.Point(42, 171);
+            this.uiInputKorisnickoIme.Multiline = true;
+            this.uiInputKorisnickoIme.Name = "uiInputKorisnickoIme";
+            this.uiInputKorisnickoIme.Size = new System.Drawing.Size(200, 30);
+            this.uiInputKorisnickoIme.TabIndex = 6;
+            // 
+            // uiClickExit
+            // 
+            this.uiClickExit.AutoSize = true;
+            this.uiClickExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.uiClickExit.Location = new System.Drawing.Point(245, 9);
+            this.uiClickExit.Name = "uiClickExit";
+            this.uiClickExit.Size = new System.Drawing.Size(24, 24);
+            this.uiClickExit.TabIndex = 12;
+            this.uiClickExit.Text = "X";
+            this.uiClickExit.Click += new System.EventHandler(this.uiClickExit_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.Gumb);
+            this.ClientSize = new System.Drawing.Size(562, 424);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
             this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button Gumb;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label uiClickExit;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button uiActionPrijaviSe;
+        private System.Windows.Forms.TextBox uiInputLozinka;
+        private System.Windows.Forms.TextBox uiInputKorisnickoIme;
     }
 }
 
--- MSDom/MSDom/Form1.cs
+++ MSDom/MSDom/Form1.cs
@@ -16,5 +16,10 @@ namespace MSDom
         {
             InitializeComponent();
         }
+
+        private void uiClickExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
     }
 }
