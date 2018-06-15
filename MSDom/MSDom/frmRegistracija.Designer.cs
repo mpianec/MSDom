d8fd8c99f4e8e4408e43b5e2992111649b8976da
kristijan-perkovic96 <kperkovic@foi.hr>
2018-06-15 21:07:45 +0200

    Kreirana forma za registraciju korisnika.

---
+++ MSDom/MSDom/frmRegistracija.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace MSDom
+{
+    partial class frmRegistracija
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.uiInputIme = new System.Windows.Forms.TextBox();
+            this.uiInputPrezime = new System.Windows.Forms.TextBox();
+            this.uiInputBrojSobe = new System.Windows.Forms.TextBox();
+            this.uiInputUloga = new System.Windows.Forms.TextBox();
+            this.uiInputLozinka = new System.Windows.Forms.TextBox();
+            this.uiInputKorisnickoIme = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.uiActionUnos = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // uiInputIme
+            // 
+            this.uiInputIme.Location = new System.Drawing.Point(200, 72);
+            this.uiInputIme.Name = "uiInputIme";
+            this.uiInputIme.Size = new System.Drawing.Size(271, 20);
+            this.uiInputIme.TabIndex = 0;
+            // 
+            // uiInputPrezime
+            // 
+            this.uiInputPrezime.Location = new System.Drawing.Point(200, 98);
+            this.uiInputPrezime.Name = "uiInputPrezime";
+            this.uiInputPrezime.Size = new System.Drawing.Size(271, 20);
+            this.uiInputPrezime.TabIndex = 1;
+            // 
+            // uiInputBrojSobe
+            // 
+            this.uiInputBrojSobe.Location = new System.Drawing.Point(200, 124);
+            this.uiInputBrojSobe.Name = "uiInputBrojSobe";
+            this.uiInputBrojSobe.Size = new System.Drawing.Size(271, 20);
+            this.uiInputBrojSobe.TabIndex = 2;
+            // 
+            // uiInputUloga
+            // 
+            this.uiInputUloga.Location = new System.Drawing.Point(200, 150);
+            this.uiInputUloga.Name = "uiInputUloga";
+            this.uiInputUloga.Size = new System.Drawing.Size(271, 20);
+            this.uiInputUloga.TabIndex = 3;
+            // 
+            // uiInputLozinka
+            // 
+            this.uiInputLozinka.Location = new System.Drawing.Point(200, 202);
+            this.uiInputLozinka.Name = "uiInputLozinka";
+            this.uiInputLozinka.Size = new System.Drawing.Size(271, 20);
+            this.uiInputLozinka.TabIndex = 4;
+            // 
+            // uiInputKorisnickoIme
+            // 
+            this.uiInputKorisnickoIme.Location = new System.Drawing.Point(200, 176);
+            this.uiInputKorisnickoIme.Name = "uiInputKorisnickoIme";
+            this.uiInputKorisnickoIme.Size = new System.Drawing.Size(271, 20);
+            this.uiInputKorisnickoIme.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Ime";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(43, 105);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Prezime";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(43, 131);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Broj sobe";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(43, 157);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Uloga";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(43, 183);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(75, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Korisničko ime";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(43, 209);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(44, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Lozinka";
+            // 
+            // uiActionUnos
+            // 
+            this.uiActionUnos.Location = new System.Drawing.Point(200, 229);
+            this.uiActionUnos.Name = "uiActionUnos";
+            this.uiActionUnos.Size = new System.Drawing.Size(271, 23);
+            this.uiActionUnos.TabIndex = 12;
+            this.uiActionUnos.Text = "Unesi";
+            this.uiActionUnos.UseVisualStyleBackColor = true;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label7.Location = new System.Drawing.Point(36, 13);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(171, 18);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Unesite novog korisnika:";
+            // 
+            // frmRegistracija
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(627, 338);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.uiActionUnos);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.uiInputKorisnickoIme);
+            this.Controls.Add(this.uiInputLozinka);
+            this.Controls.Add(this.uiInputUloga);
+            this.Controls.Add(this.uiInputBrojSobe);
+            this.Controls.Add(this.uiInputPrezime);
+            this.Controls.Add(this.uiInputIme);
+            this.Name = "frmRegistracija";
+            this.Text = "Registracija novog korisnika";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox uiInputIme;
+        private System.Windows.Forms.TextBox uiInputPrezime;
+        private System.Windows.Forms.TextBox uiInputBrojSobe;
+        private System.Windows.Forms.TextBox uiInputUloga;
+        private System.Windows.Forms.TextBox uiInputLozinka;
+        private System.Windows.Forms.TextBox uiInputKorisnickoIme;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button uiActionUnos;
+        private System.Windows.Forms.Label label7;
+    }
+}
