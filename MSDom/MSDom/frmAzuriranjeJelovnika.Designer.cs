a050538f9ed271d1b77c70fb461dd00bf63541de
kristijan-perkovic96 <kperkovic@foi.hr>
2018-06-16 14:49:32 +0200

    Kreirana forma za ažuriranje unesenog jelovnika.

---
+++ MSDom/MSDom/frmAzuriranjeJelovnika.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace MSDom
+{
+    partial class frmAzuriranjeJelovnika
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.uiActionAzuriraj = new System.Windows.Forms.Button();
+            this.uiInputKategorija = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.uiInputDesert = new System.Windows.Forms.MaskedTextBox();
+            this.uiInputPrilog = new System.Windows.Forms.MaskedTextBox();
+            this.uiInputMeso = new System.Windows.Forms.MaskedTextBox();
+            this.uiInputPredjelo = new System.Windows.Forms.MaskedTextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.uiInputID = new System.Windows.Forms.MaskedTextBox();
+            this.SuspendLayout();
+            // 
+            // uiActionAzuriraj
+            // 
+            this.uiActionAzuriraj.Location = new System.Drawing.Point(190, 172);
+            this.uiActionAzuriraj.Name = "uiActionAzuriraj";
+            this.uiActionAzuriraj.Size = new System.Drawing.Size(258, 23);
+            this.uiActionAzuriraj.TabIndex = 25;
+            this.uiActionAzuriraj.Text = "Ažuriraj";
+            this.uiActionAzuriraj.UseVisualStyleBackColor = true;
+            // 
+            // uiInputKategorija
+            // 
+            this.uiInputKategorija.FormattingEnabled = true;
+            this.uiInputKategorija.Location = new System.Drawing.Point(190, 145);
+            this.uiInputKategorija.Name = "uiInputKategorija";
+            this.uiInputKategorija.Size = new System.Drawing.Size(258, 21);
+            this.uiInputKategorija.TabIndex = 23;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(68, 152);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(54, 13);
+            this.label5.TabIndex = 22;
+            this.label5.Text = "Kategorija";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(68, 126);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 13);
+            this.label4.TabIndex = 21;
+            this.label4.Text = "Desert";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(68, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 13);
+            this.label3.TabIndex = 20;
+            this.label3.Text = "Meso";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(68, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "Prilog";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(68, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "Predjelo";
+            // 
+            // uiInputDesert
+            // 
+            this.uiInputDesert.Location = new System.Drawing.Point(190, 119);
+            this.uiInputDesert.Name = "uiInputDesert";
+            this.uiInputDesert.Size = new System.Drawing.Size(258, 20);
+            this.uiInputDesert.TabIndex = 17;
+            // 
+            // uiInputPrilog
+            // 
+            this.uiInputPrilog.Location = new System.Drawing.Point(190, 67);
+            this.uiInputPrilog.Name = "uiInputPrilog";
+            this.uiInputPrilog.Size = new System.Drawing.Size(258, 20);
+            this.uiInputPrilog.TabIndex = 16;
+            // 
+            // uiInputMeso
+            // 
+            this.uiInputMeso.Location = new System.Drawing.Point(190, 93);
+            this.uiInputMeso.Name = "uiInputMeso";
+            this.uiInputMeso.Size = new System.Drawing.Size(258, 20);
+            this.uiInputMeso.TabIndex = 15;
+            // 
+            // uiInputPredjelo
+            // 
+            this.uiInputPredjelo.Location = new System.Drawing.Point(190, 41);
+            this.uiInputPredjelo.Name = "uiInputPredjelo";
+            this.uiInputPredjelo.Size = new System.Drawing.Size(258, 20);
+            this.uiInputPredjelo.TabIndex = 14;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(68, 22);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(18, 13);
+            this.label6.TabIndex = 27;
+            this.label6.Text = "ID";
+            // 
+            // uiInputID
+            // 
+            this.uiInputID.Location = new System.Drawing.Point(190, 15);
+            this.uiInputID.Name = "uiInputID";
+            this.uiInputID.ReadOnly = true;
+            this.uiInputID.Size = new System.Drawing.Size(258, 20);
+            this.uiInputID.TabIndex = 26;
+            // 
+            // frmAzuriranjeJelovnika
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(585, 272);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.uiInputID);
+            this.Controls.Add(this.uiActionAzuriraj);
+            this.Controls.Add(this.uiInputKategorija);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.uiInputDesert);
+            this.Controls.Add(this.uiInputPrilog);
+            this.Controls.Add(this.uiInputMeso);
+            this.Controls.Add(this.uiInputPredjelo);
+            this.Name = "frmAzuriranjeJelovnika";
+            this.Text = "Ažuriranje jelovnika";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button uiActionAzuriraj;
+        private System.Windows.Forms.ComboBox uiInputKategorija;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MaskedTextBox uiInputDesert;
+        private System.Windows.Forms.MaskedTextBox uiInputPrilog;
+        private System.Windows.Forms.MaskedTextBox uiInputMeso;
+        private System.Windows.Forms.MaskedTextBox uiInputPredjelo;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.MaskedTextBox uiInputID;
+    }
+}
