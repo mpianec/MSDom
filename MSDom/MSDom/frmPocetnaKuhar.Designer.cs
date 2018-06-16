a5c41b4db40bb234d536e50a9299b73e935ba49d
kristijan-perkovic96 <kperkovic@foi.hr>
2018-06-16 14:52:52 +0200

    Kreirana početna forma koju vidi kuhar kada se prijavi u aplikaciju.

---
+++ MSDom/MSDom/frmPocetnaKuhar.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace MSDom
+{
+    partial class frmPocetnaKuhar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.uiActionKreiranjeJelovnika = new System.Windows.Forms.Button();
+            this.uiActionKreiranjeRucka = new System.Windows.Forms.Button();
+            this.uiActionKreiranjeDorucka = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.uiActionKreiranjeVecere = new System.Windows.Forms.Button();
+            this.uiActionPregledNarudzbi = new System.Windows.Forms.Button();
+            this.uiActionBrisanjeJelovnika = new System.Windows.Forms.Button();
+            this.uiActionOdjava = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // uiActionKreiranjeJelovnika
+            // 
+            this.uiActionKreiranjeJelovnika.Location = new System.Drawing.Point(116, 78);
+            this.uiActionKreiranjeJelovnika.Name = "uiActionKreiranjeJelovnika";
+            this.uiActionKreiranjeJelovnika.Size = new System.Drawing.Size(206, 23);
+            this.uiActionKreiranjeJelovnika.TabIndex = 0;
+            this.uiActionKreiranjeJelovnika.Text = "Kreiranje novog jelovnika";
+            this.uiActionKreiranjeJelovnika.UseVisualStyleBackColor = true;
+            // 
+            // uiActionKreiranjeRucka
+            // 
+            this.uiActionKreiranjeRucka.Location = new System.Drawing.Point(116, 136);
+            this.uiActionKreiranjeRucka.Name = "uiActionKreiranjeRucka";
+            this.uiActionKreiranjeRucka.Size = new System.Drawing.Size(206, 23);
+            this.uiActionKreiranjeRucka.TabIndex = 1;
+            this.uiActionKreiranjeRucka.Text = "Kreiranje ponude ručka";
+            this.uiActionKreiranjeRucka.UseVisualStyleBackColor = true;
+            // 
+            // uiActionKreiranjeDorucka
+            // 
+            this.uiActionKreiranjeDorucka.Location = new System.Drawing.Point(116, 107);
+            this.uiActionKreiranjeDorucka.Name = "uiActionKreiranjeDorucka";
+            this.uiActionKreiranjeDorucka.Size = new System.Drawing.Size(206, 23);
+            this.uiActionKreiranjeDorucka.TabIndex = 2;
+            this.uiActionKreiranjeDorucka.Text = "Kreiranje ponude doručka";
+            this.uiActionKreiranjeDorucka.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(40, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 18);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Vaše mogučnosti:";
+            // 
+            // uiActionKreiranjeVecere
+            // 
+            this.uiActionKreiranjeVecere.Location = new System.Drawing.Point(116, 165);
+            this.uiActionKreiranjeVecere.Name = "uiActionKreiranjeVecere";
+            this.uiActionKreiranjeVecere.Size = new System.Drawing.Size(206, 23);
+            this.uiActionKreiranjeVecere.TabIndex = 4;
+            this.uiActionKreiranjeVecere.Text = "Kreiranje ponude večere";
+            this.uiActionKreiranjeVecere.UseVisualStyleBackColor = true;
+            // 
+            // uiActionPregledNarudzbi
+            // 
+            this.uiActionPregledNarudzbi.Location = new System.Drawing.Point(116, 195);
+            this.uiActionPregledNarudzbi.Name = "uiActionPregledNarudzbi";
+            this.uiActionPregledNarudzbi.Size = new System.Drawing.Size(206, 23);
+            this.uiActionPregledNarudzbi.TabIndex = 5;
+            this.uiActionPregledNarudzbi.Text = "Pregled narudžbi";
+            this.uiActionPregledNarudzbi.UseVisualStyleBackColor = true;
+            // 
+            // uiActionBrisanjeJelovnika
+            // 
+            this.uiActionBrisanjeJelovnika.Location = new System.Drawing.Point(116, 225);
+            this.uiActionBrisanjeJelovnika.Name = "uiActionBrisanjeJelovnika";
+            this.uiActionBrisanjeJelovnika.Size = new System.Drawing.Size(206, 23);
+            this.uiActionBrisanjeJelovnika.TabIndex = 6;
+            this.uiActionBrisanjeJelovnika.Text = "Brisanje jelovnika";
+            this.uiActionBrisanjeJelovnika.UseVisualStyleBackColor = true;
+            // 
+            // uiActionOdjava
+            // 
+            this.uiActionOdjava.Location = new System.Drawing.Point(513, 27);
+            this.uiActionOdjava.Name = "uiActionOdjava";
+            this.uiActionOdjava.Size = new System.Drawing.Size(75, 23);
+            this.uiActionOdjava.TabIndex = 7;
+            this.uiActionOdjava.Text = "Odjava";
+            this.uiActionOdjava.UseVisualStyleBackColor = true;
+            // 
+            // frmPocetnaKuhar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(614, 348);
+            this.Controls.Add(this.uiActionOdjava);
+            this.Controls.Add(this.uiActionBrisanjeJelovnika);
+            this.Controls.Add(this.uiActionPregledNarudzbi);
+            this.Controls.Add(this.uiActionKreiranjeVecere);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.uiActionKreiranjeDorucka);
+            this.Controls.Add(this.uiActionKreiranjeRucka);
+            this.Controls.Add(this.uiActionKreiranjeJelovnika);
+            this.Name = "frmPocetnaKuhar";
+            this.Text = "Kuhar";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button uiActionKreiranjeJelovnika;
+        private System.Windows.Forms.Button uiActionKreiranjeRucka;
+        private System.Windows.Forms.Button uiActionKreiranjeDorucka;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button uiActionKreiranjeVecere;
+        private System.Windows.Forms.Button uiActionPregledNarudzbi;
+        private System.Windows.Forms.Button uiActionBrisanjeJelovnika;
+        private System.Windows.Forms.Button uiActionOdjava;
+    }
+}
