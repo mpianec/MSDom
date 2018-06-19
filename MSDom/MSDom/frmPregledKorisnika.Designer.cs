583c468a9cedb105bef6bc2ab0ae5fe961096180
kristijan-perkovic96 <kperkovic@foi.hr>
2018-06-19 19:23:00 +0200

    Kreirana forma za prikaz svih korisnika, te dodana funkcijonalnost prikaza svih korisnika.

---
+++ MSDom/MSDom/frmPregledKorisnika.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace MSDom
+{
+    partial class frmPregledKorisnika
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.uiOutputKorisnici = new System.Windows.Forms.DataGridView();
+            this.uiActionObrisi = new System.Windows.Forms.Button();
+            this.uiActionAzurirajKorisnika = new System.Windows.Forms.Button();
+            this.sobaBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.ulogaBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.korisnikBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.uiOutputKorisnici)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sobaBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ulogaBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.korisnikBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // uiOutputKorisnici
+            // 
+            this.uiOutputKorisnici.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.uiOutputKorisnici.Location = new System.Drawing.Point(13, 13);
+            this.uiOutputKorisnici.Name = "uiOutputKorisnici";
+            this.uiOutputKorisnici.Size = new System.Drawing.Size(993, 173);
+            this.uiOutputKorisnici.TabIndex = 0;
+            // 
+            // uiActionObrisi
+            // 
+            this.uiActionObrisi.Location = new System.Drawing.Point(302, 193);
+            this.uiActionObrisi.Name = "uiActionObrisi";
+            this.uiActionObrisi.Size = new System.Drawing.Size(149, 23);
+            this.uiActionObrisi.TabIndex = 1;
+            this.uiActionObrisi.Text = "Obriši korisnika";
+            this.uiActionObrisi.UseVisualStyleBackColor = true;
+            // 
+            // uiActionAzurirajKorisnika
+            // 
+            this.uiActionAzurirajKorisnika.Location = new System.Drawing.Point(457, 193);
+            this.uiActionAzurirajKorisnika.Name = "uiActionAzurirajKorisnika";
+            this.uiActionAzurirajKorisnika.Size = new System.Drawing.Size(149, 23);
+            this.uiActionAzurirajKorisnika.TabIndex = 2;
+            this.uiActionAzurirajKorisnika.Text = "Ažuriraj korisnika";
+            this.uiActionAzurirajKorisnika.UseVisualStyleBackColor = true;
+            // 
+            // sobaBindingSource
+            // 
+            this.sobaBindingSource.DataSource = typeof(MSDom.soba);
+            // 
+            // ulogaBindingSource
+            // 
+            this.ulogaBindingSource.DataSource = typeof(MSDom.uloga);
+            // 
+            // korisnikBindingSource
+            // 
+            this.korisnikBindingSource.DataSource = typeof(MSDom.korisnik);
+            // 
+            // frmPregledKorisnika
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1018, 282);
+            this.Controls.Add(this.uiActionAzurirajKorisnika);
+            this.Controls.Add(this.uiActionObrisi);
+            this.Controls.Add(this.uiOutputKorisnici);
+            this.Name = "frmPregledKorisnika";
+            this.Text = "Pregled korisnika";
+            ((System.ComponentModel.ISupportInitialize)(this.uiOutputKorisnici)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sobaBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ulogaBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.korisnikBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView uiOutputKorisnici;
+        private System.Windows.Forms.Button uiActionObrisi;
+        private System.Windows.Forms.Button uiActionAzurirajKorisnika;
+        private System.Windows.Forms.BindingSource korisnikBindingSource;
+        private System.Windows.Forms.BindingSource sobaBindingSource;
+        private System.Windows.Forms.BindingSource ulogaBindingSource;
+    }
+}
---
+++ MSDom/MSDom/frmPregledKorisnika.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MSDom
+{
+    public partial class frmPregledKorisnika : Form
+    {
+        public frmPregledKorisnika()
+        {
+            InitializeComponent();
+            DohvatiKorisnike();
+        }
+
+        public void DohvatiKorisnike()
+        {
+            
+            using (var db=new MSDomEntities())
+            {
+                var listaKorisnika = from kor in db.korisniks
+                                     join ulo in db.ulogas
+                                     on kor.ulogaId equals ulo.id
+                                     join sob in db.sobas
+                                     on kor.sobaId equals sob.id
+                                     select new {kor.id,kor.ime,kor.prezime,kor.korisnickoIme,kor.lozinka,ulo.naziv, sob.brojSobe};
+                uiOutputKorisnici.DataSource = listaKorisnika.ToList();
+            }
+            
+        }
+    }
+}
