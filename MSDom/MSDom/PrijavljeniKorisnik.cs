f6c56f59bb283390b377f02e8a7c9d658b304379
kristijan-perkovic96 <kperkovic@foi.hr>
2018-06-19 17:20:57 +0200

    Kreirana klasa PrijavljeniKorisnik.cs u koju se spremaju podaci o prijavljenom korisniku.

---
+++ MSDom/MSDom/PrijavljeniKorisnik.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSDom
+{
+    public static class PrijavljeniKorisnik
+    {
+        public static int id { get; set; }
+        public static string ime { get; set; }
+        public static string prezime { get; set; }
+        public static int? idSobe { get; set; }
+        public static string korisnickoIme { get; set; }
+        public static string lozinka { get; set; }
+        public static int uloga { get; set; }
+
+    }
+}
