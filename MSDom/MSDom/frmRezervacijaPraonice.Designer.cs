2b4ff92cfcef0a64c61e9ff3041d35515318ae4f
mpianec <mpianec@foi.hr>
2018-06-22 18:17:53 +0200

    Kreirana forma za rezervaciju praonice.

---
+++ MSDom/MSDom/frmRezervacijaPraonice.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace MSDom
+{
+    partial class frmRezervacijaPraonice
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.uiActionRezervirajPraonicu = new System.Windows.Forms.Button();
+            this.uiOutputSlobodnePraonice = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.uiOutputMojeRezervacije = new System.Windows.Forms.DataGridView();
+            this.uiActionPoništiRezervaciju = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.uiOutputSlobodnePraonice)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.uiOutputMojeRezervacije)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // uiActionRezervirajPraonicu
+            // 
+            this.uiActionRezervirajPraonicu.Location = new System.Drawing.Point(432, 42);
+            this.uiActionRezervirajPraonicu.Name = "uiActionRezervirajPraonicu";
+            this.uiActionRezervirajPraonicu.Size = new System.Drawing.Size(75, 23);
+            this.uiActionRezervirajPraonicu.TabIndex = 0;
+            this.uiActionRezervirajPraonicu.Text = "Rezerviraj";
+            this.uiActionRezervirajPraonicu.UseVisualStyleBackColor = true;
+            // 
+            // uiOutputSlobodnePraonice
+            // 
+            this.uiOutputSlobodnePraonice.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.uiOutputSlobodnePraonice.Location = new System.Drawing.Point(19, 42);
+            this.uiOutputSlobodnePraonice.Name = "uiOutputSlobodnePraonice";
+            this.uiOutputSlobodnePraonice.Size = new System.Drawing.Size(393, 150);
+            this.uiOutputSlobodnePraonice.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Slobodne praonice";
+            // 
+            // uiOutputMojeRezervacije
+            // 
+            this.uiOutputMojeRezervacije.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.uiOutputMojeRezervacije.Location = new System.Drawing.Point(19, 219);
+            this.uiOutputMojeRezervacije.Name = "uiOutputMojeRezervacije";
+            this.uiOutputMojeRezervacije.Size = new System.Drawing.Size(393, 150);
+            this.uiOutputMojeRezervacije.TabIndex = 3;
+            // 
+            // uiActionPoništiRezervaciju
+            // 
+            this.uiActionPoništiRezervaciju.Location = new System.Drawing.Point(432, 323);
+            this.uiActionPoništiRezervaciju.Name = "uiActionPoništiRezervaciju";
+            this.uiActionPoništiRezervaciju.Size = new System.Drawing.Size(75, 23);
+            this.uiActionPoništiRezervaciju.TabIndex = 4;
+            this.uiActionPoništiRezervaciju.Text = "Poništi rezervaciju";
+            this.uiActionPoništiRezervaciju.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 203);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Moje rezervacije";
+            // 
+            // frmRezervacijaPraonice
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(636, 399);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.uiActionPoništiRezervaciju);
+            this.Controls.Add(this.uiOutputMojeRezervacije);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.uiOutputSlobodnePraonice);
+            this.Controls.Add(this.uiActionRezervirajPraonicu);
+            this.Name = "frmRezervacijaPraonice";
+            this.Text = "frmRezervacijaPraonice";
+            ((System.ComponentModel.ISupportInitialize)(this.uiOutputSlobodnePraonice)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.uiOutputMojeRezervacije)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button uiActionRezervirajPraonicu;
+        private System.Windows.Forms.DataGridView uiOutputSlobodnePraonice;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView uiOutputMojeRezervacije;
+        private System.Windows.Forms.Button uiActionPoništiRezervaciju;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ MSDom/MSDom/frmRezervacijaPraonice.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MSDom
+{
+    public partial class frmRezervacijaPraonice : Form
+    {
+        public frmRezervacijaPraonice()
+        {
+            InitializeComponent();
+            DohvatiSlobodnePraonice();
+        }
+        public void DohvatiSlobodnePraonice()
+        {
+            using (var db = new MSDomEntities())
+            {
+                var listaSlobodnihPraonica = from praon in db.praonicas
+                                            where (praon.zauzeto == 0)
+                                            select new { praon.id, praon.naziv};
+
+                uiOutputSlobodnePraonice.DataSource = listaSlobodnihPraonica.ToList();
+            }
+        }
+    }
+}
