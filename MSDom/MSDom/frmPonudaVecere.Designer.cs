78825251aa85a93f00f8030c73ea76dbb1f93060
kristijan-perkovic96 <kperkovic@foi.hr>
2018-06-22 16:46:33 +0200

    Na fromi za odabir večere omogućena funkcionalnost za odabir.

--- MSDom/MSDom/frmPonudaVecere.Designer.cs
+++ MSDom/MSDom/frmPonudaVecere.Designer.cs
@@ -31,47 +31,86 @@
             this.uiActionPonistiOdabir = new System.Windows.Forms.Button();
             this.uiActionSpremiOdabir = new System.Windows.Forms.Button();
             this.uiOutputPonudaVecere = new System.Windows.Forms.DataGridView();
+            this.uiOutputVasIzbor = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.uiOutputPonudaVecere)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.uiOutputVasIzbor)).BeginInit();
             this.SuspendLayout();
             // 
             // uiActionPonistiOdabir
             // 
-            this.uiActionPonistiOdabir.Location = new System.Drawing.Point(335, 214);
+            this.uiActionPonistiOdabir.Location = new System.Drawing.Point(533, 340);
             this.uiActionPonistiOdabir.Name = "uiActionPonistiOdabir";
             this.uiActionPonistiOdabir.Size = new System.Drawing.Size(174, 23);
             this.uiActionPonistiOdabir.TabIndex = 5;
             this.uiActionPonistiOdabir.Text = "Poništi moj odabir";
             this.uiActionPonistiOdabir.UseVisualStyleBackColor = true;
+            this.uiActionPonistiOdabir.Click += new System.EventHandler(this.uiActionPonistiOdabir_Click);
             // 
             // uiActionSpremiOdabir
             // 
-            this.uiActionSpremiOdabir.Location = new System.Drawing.Point(61, 214);
+            this.uiActionSpremiOdabir.Location = new System.Drawing.Point(533, 208);
             this.uiActionSpremiOdabir.Name = "uiActionSpremiOdabir";
             this.uiActionSpremiOdabir.Size = new System.Drawing.Size(174, 23);
             this.uiActionSpremiOdabir.TabIndex = 4;
             this.uiActionSpremiOdabir.Text = "Spremi odabir";
             this.uiActionSpremiOdabir.UseVisualStyleBackColor = true;
+            this.uiActionSpremiOdabir.Click += new System.EventHandler(this.uiActionSpremiOdabir_Click);
             // 
             // uiOutputPonudaVecere
             // 
             this.uiOutputPonudaVecere.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.uiOutputPonudaVecere.Location = new System.Drawing.Point(61, 40);
             this.uiOutputPonudaVecere.Name = "uiOutputPonudaVecere";
-            this.uiOutputPonudaVecere.Size = new System.Drawing.Size(448, 160);
+            this.uiOutputPonudaVecere.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.uiOutputPonudaVecere.Size = new System.Drawing.Size(646, 162);
             this.uiOutputPonudaVecere.TabIndex = 3;
+            // 
+            // uiOutputVasIzbor
+            // 
+            this.uiOutputVasIzbor.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.uiOutputVasIzbor.Location = new System.Drawing.Point(61, 251);
+            this.uiOutputVasIzbor.Name = "uiOutputVasIzbor";
+            this.uiOutputVasIzbor.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.uiOutputVasIzbor.Size = new System.Drawing.Size(646, 83);
+            this.uiOutputVasIzbor.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(61, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Današnja ponuda";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(61, 232);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Vaš izbor";
             // 
             // frmPonudaVecere
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(603, 293);
+            this.ClientSize = new System.Drawing.Size(757, 400);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.uiOutputVasIzbor);
             this.Controls.Add(this.uiActionPonistiOdabir);
             this.Controls.Add(this.uiActionSpremiOdabir);
             this.Controls.Add(this.uiOutputPonudaVecere);
             this.Name = "frmPonudaVecere";
             this.Text = "Ponuda večere";
             ((System.ComponentModel.ISupportInitialize)(this.uiOutputPonudaVecere)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.uiOutputVasIzbor)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -80,5 +119,8 @@
         private System.Windows.Forms.Button uiActionPonistiOdabir;
         private System.Windows.Forms.Button uiActionSpremiOdabir;
         private System.Windows.Forms.DataGridView uiOutputPonudaVecere;
+        private System.Windows.Forms.DataGridView uiOutputVasIzbor;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
--- MSDom/MSDom/frmPonudaVecere.cs
+++ MSDom/MSDom/frmPonudaVecere.cs
@@ -15,6 +15,131 @@ namespace MSDom
         public frmPonudaVecere()
         {
             InitializeComponent();
+            DohvatiOdabreneVecere();
+            DohvatiVasOdabir();
+        }
+
+        private void DohvatiOdabreneVecere()
+        {
+            using (var db = new MSDomEntities())
+            {
+                var listaAktivnihRucka = from dor in db.izbors
+                                         where (dor.kategorijaId == 3 && dor.aktivanIzbor == 1)
+                                         select new { dor.id, dor.predjelo, dor.prilog, dor.meso, dor.desert };
+
+                uiOutputPonudaVecere.DataSource = listaAktivnihRucka.ToList();
+            }
+        }
+        private void DohvatiVasOdabir()
+        {
+            DateTime datum = DateTime.Today;
+            using (var db = new MSDomEntities())
+            {
+
+                var idOdabranogJelovnika = from od in db.vasIzborDanas
+                                           join dor in db.izbors
+                                           on od.izborId equals dor.id
+                                           where (od.datum == datum && od.korisnikId == PrijavljeniKorisnik.id && dor.kategorijaId == 3)
+                                           select new { dor.id, dor.predjelo, dor.prilog, dor.meso, dor.desert, dor.kategorijaId };
+                if (idOdabranogJelovnika.ToList().Count > 0)
+                {
+                    uiOutputVasIzbor.DataSource = idOdabranogJelovnika.ToList();
+                }
+                else
+                {
+                    uiOutputVasIzbor.DataSource = null;
+                }
+
+            }
+        }
+
+        private void uiActionSpremiOdabir_Click(object sender, EventArgs e)
+        {
+            if (uiOutputPonudaVecere.CurrentRow != null)
+            {
+                bool postojiOdabraniDorucak = false;
+                int idJelovnika = int.Parse(uiOutputPonudaVecere.CurrentRow.Cells[0].Value.ToString());
+                DateTime datum = DateTime.Today;
+                using (var db = new MSDomEntities())
+                {
+                    var idOdabranogJelovnika = from od in db.vasIzborDanas
+                                               join dor in db.izbors
+                                               on od.izborId equals dor.id
+                                               where (od.datum == datum && od.korisnikId == PrijavljeniKorisnik.id)
+                                               select new { dor.id, dor.predjelo, dor.prilog, dor.meso, dor.desert, dor.kategorijaId };
+                    if (idOdabranogJelovnika.ToList().Count == 0)
+                    {
+                        vasIzborDana noviIzborDana = new vasIzborDana();
+                        noviIzborDana.datum = datum;
+                        noviIzborDana.korisnikId = PrijavljeniKorisnik.id;
+                        noviIzborDana.izborId = idJelovnika;
+                        db.vasIzborDanas.Add(noviIzborDana);
+                        db.SaveChanges();
+                    }
+                    else
+                    {
+                        foreach (var item in idOdabranogJelovnika.ToList())
+                        {
+                            if (item.kategorijaId == 3)
+                            {
+                                postojiOdabraniDorucak = true;
+                            }
+                        }
+                        if (!postojiOdabraniDorucak)
+                        {
+                            vasIzborDana noviIzborDana = new vasIzborDana();
+                            noviIzborDana.datum = datum;
+                            noviIzborDana.korisnikId = PrijavljeniKorisnik.id;
+                            noviIzborDana.izborId = idJelovnika;
+                            db.vasIzborDanas.Add(noviIzborDana);
+                            db.SaveChanges();
+                        }
+                        else
+                        {
+                            MessageBox.Show("Imate odabrani ručak. Poništite trenutni izbor.");
+                        }
+
+                    }
+                }
+            }
+            DohvatiOdabreneVecere();
+            DohvatiVasOdabir();
+        }
+
+        private void uiActionPonistiOdabir_Click(object sender, EventArgs e)
+        {
+            vasIzborDana izborDanaZaBrisanje = null;
+            DateTime datum = DateTime.Today;
+            using (var db = new MSDomEntities())
+            {
+                var idOdabira = from v in db.vasIzborDanas
+                                join dor in db.izbors
+                                on v.izborId equals dor.id
+                                where (v.datum == datum && v.korisnikId == PrijavljeniKorisnik.id && dor.kategorijaId==3)
+                                select v.id;
+                if (idOdabira.ToList().Count > 0)
+                {
+                    int id = Convert.ToInt32(idOdabira.FirstOrDefault());
+
+                    BindingList<vasIzborDana> listaIzboraDana = new BindingList<vasIzborDana>(db.vasIzborDanas.ToList());
+                    foreach (var item in listaIzboraDana)
+                    {
+                        if (item.id == id)
+                        {
+                            izborDanaZaBrisanje = item;
+                        }
+                    }
+                    db.vasIzborDanas.Attach(izborDanaZaBrisanje);
+                    db.vasIzborDanas.Remove(izborDanaZaBrisanje);
+                    db.SaveChanges();
+                }
+                else
+                {
+                    MessageBox.Show("Nemate dodan izbor za večeru.");
+                }
+            }
+            DohvatiOdabreneVecere();
+            DohvatiVasOdabir();
         }
     }
 }
