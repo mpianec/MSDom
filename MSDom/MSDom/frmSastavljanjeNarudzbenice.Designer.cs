5559e97de899be3b2cbc95ad4e14b80b898cfbe8
tinozyeah <vpoljak@foi.hr>
2018-06-23 23:53:11 +0200

    Kreirana forma za sastavljanje narudžbenice za lijekove

---
+++ MSDom/MSDom/frmSastavljanjeNarudzbenice.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace MSDom
+{
+    partial class frmSastavljanjeNarudzbenice
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.uiActionPosaljiDoktoru = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(136, 52);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(180, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(136, 91);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(180, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(66, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Naziv lijeka:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(66, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Količina:";
+            // 
+            // uiActionPosaljiDoktoru
+            // 
+            this.uiActionPosaljiDoktoru.Location = new System.Drawing.Point(151, 147);
+            this.uiActionPosaljiDoktoru.Name = "uiActionPosaljiDoktoru";
+            this.uiActionPosaljiDoktoru.Size = new System.Drawing.Size(154, 32);
+            this.uiActionPosaljiDoktoru.TabIndex = 4;
+            this.uiActionPosaljiDoktoru.Text = "Pošalji doktoru na pregled";
+            this.uiActionPosaljiDoktoru.UseVisualStyleBackColor = true;
+            // 
+            // frmSastavljanjeNarudzbenice
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(443, 270);
+            this.Controls.Add(this.uiActionPosaljiDoktoru);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "frmSastavljanjeNarudzbenice";
+            this.Text = "frmSastavljanjeNarudzbenice";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button uiActionPosaljiDoktoru;
+    }
+}
