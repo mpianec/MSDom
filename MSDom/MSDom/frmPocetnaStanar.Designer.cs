b9ec13077b4290526420a2dfd9fccf0b9c7ff2af
mpianec <mpianec@foi.hr>
2018-06-25 17:57:28 +0200

    Dodana prijava za spremačicu.

--- MSDom/MSDom/frmPocetnaStanar.Designer.cs
+++ MSDom/MSDom/frmPocetnaStanar.Designer.cs
@@ -74,7 +74,7 @@
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(127, 18);
             this.label1.TabIndex = 4;
-            this.label1.Text = "Veše mogučnosti:";
+            this.label1.Text = "Vaše mogučnosti:";
             // 
             // uiActionRezervacijaPraonice
             // 
