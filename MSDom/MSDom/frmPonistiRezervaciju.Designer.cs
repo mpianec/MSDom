87531071b1ab2bf245bcb2443abab888fb703ce8
mpianec <mpianec@foi.hr>
2018-06-25 17:57:06 +0200

    Dodana forma za poništavanje rezervacija praonica.

---
+++ MSDom/MSDom/frmPonistiRezervaciju.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace MSDom
+{
+    partial class frmPonistiRezervaciju
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.uiOutputRezervacije = new System.Windows.Forms.DataGridView();
+            this.uiActionPonisti = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.uiOutputRezervacije)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // uiOutputRezervacije
+            // 
+            this.uiOutputRezervacije.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.uiOutputRezervacije.Location = new System.Drawing.Point(19, 61);
+            this.uiOutputRezervacije.Name = "uiOutputRezervacije";
+            this.uiOutputRezervacije.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.uiOutputRezervacije.Size = new System.Drawing.Size(480, 150);
+            this.uiOutputRezervacije.TabIndex = 0;
+            // 
+            // uiActionPonisti
+            // 
+            this.uiActionPonisti.Location = new System.Drawing.Point(222, 245);
+            this.uiActionPonisti.Name = "uiActionPonisti";
+            this.uiActionPonisti.Size = new System.Drawing.Size(122, 23);
+            this.uiActionPonisti.TabIndex = 1;
+            this.uiActionPonisti.Text = "Poništi rezervaciju";
+            this.uiActionPonisti.UseVisualStyleBackColor = true;
+            this.uiActionPonisti.Click += new System.EventHandler(this.uiActionPonisti_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Rezervacije praonica: ";
+            // 
+            // frmPonistiRezervaciju
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(572, 304);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.uiActionPonisti);
+            this.Controls.Add(this.uiOutputRezervacije);
+            this.Name = "frmPonistiRezervaciju";
+            this.Text = "Poništavanje rezervacije";
+            ((System.ComponentModel.ISupportInitialize)(this.uiOutputRezervacije)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView uiOutputRezervacije;
+        private System.Windows.Forms.Button uiActionPonisti;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ MSDom/MSDom/frmPonistiRezervaciju.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MSDom
+{
+    public partial class frmPonistiRezervaciju : Form
+    {
+        public frmPonistiRezervaciju()
+        {
+            InitializeComponent();
+            DohvatiRezervirane();
+        }
+        public void DohvatiRezervirane()
+        {
+            using (var db=new MSDomEntities())
+            {
+                var idRezervacije = from rez in db.rezervacijaPraonices
+                                    join praon in db.praonicas
+                                    on rez.praonicaId equals praon.id
+                                    select new { rez.id, praon.naziv, rez.datumVrijeme };
+                if(idRezervacije.ToList().Count>0)
+                    uiOutputRezervacije.DataSource = idRezervacije.ToList();
+                else
+                    uiOutputRezervacije.DataSource = null;
+            }
+        }
+
+        private void uiActionPonisti_Click(object sender, EventArgs e)
+        {
+            rezervacijaPraonice brisanjeRezervacije = null;
+            praonica praonicaBrisanje = null;
+            using (var db=new MSDomEntities())
+            {
+                var idOdabir = from od in db.rezervacijaPraonices
+                               join praon in db.praonicas
+                               on od.praonicaId equals praon.id
+                               select od.id;
+                if (idOdabir.ToList().Count > 0)
+                {
+                    int id = int.Parse(uiOutputRezervacije.CurrentRow.Cells[0].Value.ToString());
+                    string naziv = uiOutputRezervacije.CurrentRow.Cells[1].Value.ToString();
+                    BindingList<praonica> listaPraonica = new BindingList<praonica>(db.praonicas.ToList());
+                    BindingList<rezervacijaPraonice> listaOdabira = new BindingList<rezervacijaPraonice>(db.rezervacijaPraonices.ToList());
+                    foreach (var item in listaOdabira)
+                    {
+                        if (item.id == id)
+                        {
+                            brisanjeRezervacije = item;
+                        }
+                    }
+                    foreach (var item in listaPraonica)
+                    {
+                        if (item.naziv == naziv)
+                        {
+                            praonicaBrisanje = item;
+                        }
+                    }
+                    db.praonicas.Attach(praonicaBrisanje);
+                    praonicaBrisanje.zauzeto = 0;
+                    db.rezervacijaPraonices.Attach(brisanjeRezervacije);
+                    db.rezervacijaPraonices.Remove(brisanjeRezervacije);
+                    db.SaveChanges();
+                }
+                else
+                {
+                    MessageBox.Show("Ne možete brisati.");
+                }
+            }
+            DohvatiRezervirane();
+        }
+    }
+}
