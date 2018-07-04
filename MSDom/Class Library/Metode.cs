991becbbf142f20be8294166ecbf1e89a694f2b3
mpianec <mpianec@foi.hr>
2018-07-04 22:22:19 +0200

    Dodan Class Library te je referenciran u projektu i u kodu.

---
+++ MSDom/Class Library/Metode.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Library
+{
+    public class Metode
+    {
+        public void DohvatiRezervirane()
+        {
+            using (var db = new MSDomEntities())
+            {
+                var idRezervacije = from rez in db.rezervacijaPraonices
+                                    join praon in db.praonicas
+                                    on rez.praonicaId equals praon.id
+                                    select new { rez.id, praon.naziv, rez.datumVrijeme };                
+            }
+        }
+    }
+}
