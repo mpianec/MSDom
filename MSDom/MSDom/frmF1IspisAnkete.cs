9774072f0115ccc9e024ddbf944c8e7ccff82273
mpianec <mpianec@foi.hr>
2018-07-05 22:20:10 +0200

    Dodane upute pritiskom na F1 za formu IspisAnkete.

---
+++ MSDom/MSDom/frmF1IspisAnkete.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MSDom
+{
+    public partial class frmF1IspisAnkete : Form
+    {
+        public frmF1IspisAnkete()
+        {
+            InitializeComponent();
+            label1.Text = "Za ispis ankete odaberite anketu iz padajućeg izbornika.\n";
+            label1.Text += "Kada se anketa učita pritisnite sličicu 'Printera' te će se otvorit\n";
+            label1.Text += "izbornik s postavkama ispisa";
+        }
+    }
+}
