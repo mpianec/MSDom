d0c8fc282dd55b6016738245119fd0065cd6eff6
mpianec <mpianec@foi.hr>
2018-07-05 22:20:57 +0200

    Dodane upute pritiskom na F1 za formu PoništiRezervaciju.

---
+++ MSDom/MSDom/frmF1PoništiRezervaciju.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MSDom
+{
+    public partial class frmF1PoništiRezervaciju : Form
+    {
+        public frmF1PoništiRezervaciju()
+        {
+            InitializeComponent();
+            label1.Text = "Odaberite željenu praonicu klikom na nju. \nDa bi ste poništili rezervaciju pristisnite gumb 'Poništi rezervaciju'.";
+        }
+    }
+}
