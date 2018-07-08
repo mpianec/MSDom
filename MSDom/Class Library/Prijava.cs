e761a65f5dd8eca633583f226c0954fdeffe337e
tinozyeah <vpoljak@foi.hr>
2018-07-08 16:35:30 +0200

    Promijenjeno ime dll-a

---
+++ MSDom/Class Library/Prijava.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Library
+{
+    public class Prijava
+    {
+        public string KorisnickoIme { get; set; }
+        public string Lozinka { get; set; }
+        public Prijava()
+        {
+
+        }
+
+        public bool UneseneVrijednosti(string korIme, string lozinka)
+        {
+            if (korIme == "")
+            {
+                return false;
+                
+            }
+            else if (lozinka == "")
+            {
+                return false;
+            }
+            return true;
+        }
+
+
+    }
+}
