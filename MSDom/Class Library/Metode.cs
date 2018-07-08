c5275d57cb13feb171e13cbd45489ed373692b97
tinozyeah <vpoljak@foi.hr>
2018-07-08 11:06:55 +0200

    Izmjene u dll-u

--- MSDom/Class Library/Metode.cs
+++ MSDom/Class Library/Metode.cs
@@ -8,18 +8,18 @@ namespace Class_Library
 {
     public class Metode
     {
+
         public IEnumerable<object> DohvatiRezervirane1()
         {
-            
             List<object> lista = new List<object>();
-            using (var db = new MSDomEntities())
-            {
-                IEnumerable<object> idRezervacije = from rez in db.rezervacijaPraonice
-                                           join praon in db.praonica
-                                           on rez.praonicaId equals praon.id
-                                           select new { rez.id, praon.naziv, rez.datumVrijeme };
-                lista = new List<object>(idRezervacije);
-            }
+            var db = new MSDomEntities();
+            
+            IEnumerable<object> idRezervacije = from rez in db.rezervacijaPraonice
+                                        join praon in db.praonica
+                                        on rez.praonicaId equals praon.id
+                                        select new { rez.id, praon.naziv, rez.datumVrijeme };
+            lista = new List<object>(idRezervacije);
+            
             return lista;
         }
     }
