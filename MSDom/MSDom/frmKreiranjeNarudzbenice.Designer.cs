57ea3b18ba8616a47ffe3d9787b4c959c741222d
tinozyeah <vpoljak@foi.hr>
2019-02-05 14:56:55 +0100

    Omogućene funkcionalnosti kreiranje, ažuriranje i brisanje narudžbenica

--- MSDom/MSDom/frmKreiranjeNarudzbenice.Designer.cs
+++ MSDom/MSDom/frmKreiranjeNarudzbenice.Designer.cs
@@ -33,27 +33,17 @@
             this.uiActionKreirajNarudzbenicu = new System.Windows.Forms.Button();
             this.uiActionIspis = new System.Windows.Forms.Button();
             this.uiActionDodajStavke = new System.Windows.Forms.Button();
-            this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.korisnikIdDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.datumDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.korisnikDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.stavkeNarudzbenicesDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.uiActionObrisi = new System.Windows.Forms.Button();
             this.narudzbenicaBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.korisnikBindingSource = new System.Windows.Forms.BindingSource(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.narudzbenicaBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.korisnikBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
-            this.dataGridView1.AutoGenerateColumns = false;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.idDataGridViewTextBoxColumn,
-            this.korisnikIdDataGridViewTextBoxColumn,
-            this.datumDataGridViewTextBoxColumn,
-            this.korisnikDataGridViewTextBoxColumn,
-            this.stavkeNarudzbenicesDataGridViewTextBoxColumn});
-            this.dataGridView1.DataSource = this.narudzbenicaBindingSource;
             this.dataGridView1.Location = new System.Drawing.Point(12, 29);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(675, 253);
@@ -71,16 +61,17 @@
             // 
             // uiActionIspis
             // 
-            this.uiActionIspis.Location = new System.Drawing.Point(177, 288);
+            this.uiActionIspis.Location = new System.Drawing.Point(167, 288);
             this.uiActionIspis.Name = "uiActionIspis";
             this.uiActionIspis.Size = new System.Drawing.Size(75, 23);
             this.uiActionIspis.TabIndex = 2;
             this.uiActionIspis.Text = "Ispis";
             this.uiActionIspis.UseVisualStyleBackColor = true;
+            this.uiActionIspis.Click += new System.EventHandler(this.uiActionIspis_Click);
             // 
             // uiActionDodajStavke
             // 
-            this.uiActionDodajStavke.Location = new System.Drawing.Point(321, 288);
+            this.uiActionDodajStavke.Location = new System.Drawing.Point(323, 288);
             this.uiActionDodajStavke.Name = "uiActionDodajStavke";
             this.uiActionDodajStavke.Size = new System.Drawing.Size(89, 23);
             this.uiActionDodajStavke.TabIndex = 3;
@@ -88,47 +79,30 @@
             this.uiActionDodajStavke.UseVisualStyleBackColor = true;
             this.uiActionDodajStavke.Click += new System.EventHandler(this.uiActionDodajStavke_Click);
             // 
-            // idDataGridViewTextBoxColumn
+            // uiActionObrisi
             // 
-            this.idDataGridViewTextBoxColumn.DataPropertyName = "id";
-            this.idDataGridViewTextBoxColumn.HeaderText = "id";
-            this.idDataGridViewTextBoxColumn.Name = "idDataGridViewTextBoxColumn";
-            // 
-            // korisnikIdDataGridViewTextBoxColumn
-            // 
-            this.korisnikIdDataGridViewTextBoxColumn.DataPropertyName = "korisnikId";
-            this.korisnikIdDataGridViewTextBoxColumn.HeaderText = "korisnikId";
-            this.korisnikIdDataGridViewTextBoxColumn.Name = "korisnikIdDataGridViewTextBoxColumn";
-            // 
-            // datumDataGridViewTextBoxColumn
-            // 
-            this.datumDataGridViewTextBoxColumn.DataPropertyName = "datum";
-            this.datumDataGridViewTextBoxColumn.HeaderText = "datum";
-            this.datumDataGridViewTextBoxColumn.Name = "datumDataGridViewTextBoxColumn";
-            // 
-            // korisnikDataGridViewTextBoxColumn
-            // 
-            this.korisnikDataGridViewTextBoxColumn.DataPropertyName = "korisnik";
-            this.korisnikDataGridViewTextBoxColumn.HeaderText = "korisnik";
-            this.korisnikDataGridViewTextBoxColumn.Name = "korisnikDataGridViewTextBoxColumn";
-            this.korisnikDataGridViewTextBoxColumn.Visible = false;
-            // 
-            // stavkeNarudzbenicesDataGridViewTextBoxColumn
-            // 
-            this.stavkeNarudzbenicesDataGridViewTextBoxColumn.DataPropertyName = "stavkeNarudzbenices";
-            this.stavkeNarudzbenicesDataGridViewTextBoxColumn.HeaderText = "stavkeNarudzbenices";
-            this.stavkeNarudzbenicesDataGridViewTextBoxColumn.Name = "stavkeNarudzbenicesDataGridViewTextBoxColumn";
-            this.stavkeNarudzbenicesDataGridViewTextBoxColumn.Visible = false;
+            this.uiActionObrisi.Location = new System.Drawing.Point(498, 288);
+            this.uiActionObrisi.Name = "uiActionObrisi";
+            this.uiActionObrisi.Size = new System.Drawing.Size(75, 23);
+            this.uiActionObrisi.TabIndex = 4;
+            this.uiActionObrisi.Text = "Obriši";
+            this.uiActionObrisi.UseVisualStyleBackColor = true;
+            this.uiActionObrisi.Click += new System.EventHandler(this.uiActionObrisi_Click);
             // 
             // narudzbenicaBindingSource
             // 
             this.narudzbenicaBindingSource.DataSource = typeof(MSDom.narudzbenica);
+            // 
+            // korisnikBindingSource
+            // 
+            this.korisnikBindingSource.DataSource = typeof(MSDom.korisnik);
             // 
             // frmKreiranjeNarudzbenice
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.uiActionObrisi);
             this.Controls.Add(this.uiActionDodajStavke);
             this.Controls.Add(this.uiActionIspis);
             this.Controls.Add(this.uiActionKreirajNarudzbenicu);
@@ -137,6 +111,7 @@
             this.Text = "frmKreiranjeNarudzbenice";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.narudzbenicaBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.korisnikBindingSource)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -144,14 +119,11 @@
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
-        private System.Windows.Forms.DataGridViewTextBoxColumn korisnikIdDataGridViewTextBoxColumn;
-        private System.Windows.Forms.DataGridViewTextBoxColumn datumDataGridViewTextBoxColumn;
-        private System.Windows.Forms.DataGridViewTextBoxColumn korisnikDataGridViewTextBoxColumn;
-        private System.Windows.Forms.DataGridViewTextBoxColumn stavkeNarudzbenicesDataGridViewTextBoxColumn;
         private System.Windows.Forms.BindingSource narudzbenicaBindingSource;
         private System.Windows.Forms.Button uiActionKreirajNarudzbenicu;
         private System.Windows.Forms.Button uiActionIspis;
         private System.Windows.Forms.Button uiActionDodajStavke;
+        private System.Windows.Forms.BindingSource korisnikBindingSource;
+        private System.Windows.Forms.Button uiActionObrisi;
     }
 }
--- MSDom/MSDom/frmKreiranjeNarudzbenice.cs
+++ MSDom/MSDom/frmKreiranjeNarudzbenice.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using IspisPdf;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,7 +13,8 @@ namespace MSDom
 {
     public partial class frmKreiranjeNarudzbenice : Form
     {
-        int idNarudzbenice;
+        int posljednjiId;
+        int? idNarudzbenice ;
         public frmKreiranjeNarudzbenice()
         {
             InitializeComponent();
@@ -21,37 +23,128 @@ namespace MSDom
 
         public void DohvatiNarudzbenice()
         {
-            BindingList<narudzbenica> listaNarudzbenica = null;
-
+           
             using (var db = new MSDomEntities())
             {
 
-                listaNarudzbenica = new BindingList<narudzbenica>(db.narudzbenicas.ToList());
+                var listaNarudzbenica = from nar in db.narudzbenicas
+                                    join kor in db.korisniks
+                                    on nar.korisnikId equals kor.id
+                                    select new { nar.id, kor.ime, kor.prezime, nar.datum };
 
                 var listica = listaNarudzbenica.OrderBy(x => x.id).ToList();
-                narudzbenicaBindingSource.DataSource = listica;
+                dataGridView1.DataSource = null;
+                dataGridView1.DataSource = listica;
+                posljednjiId = listica[listica.ToArray().Length-1].id+1;
 
             }
         }
 
         private void uiActionKreirajNarudzbenicu_Click(object sender, EventArgs e)
         {
-
+            using (var db= new MSDomEntities())
+            {
+                narudzbenica n = new narudzbenica
+                {
+                    id = posljednjiId,
+                    korisnikId = PrijavljeniKorisnik.id,
+                    datum = DateTime.Now
+                };
+                db.narudzbenicas.Add(n);
+                db.SaveChanges();
+            }
+            DohvatiNarudzbenice();
         }
 
         private void uiActionDodajStavke_Click(object sender, EventArgs e)
         {
-
-            narudzbenica n = narudzbenicaBindingSource.Current as narudzbenica;
-            if (n!=null)
+            idNarudzbenice = int.Parse(dataGridView1.CurrentRow.Cells[0].Value.ToString());
+            if (idNarudzbenice!=null)
             {
-                idNarudzbenice = n.id;
-                frmStavkeNarudzbenice forma = new frmStavkeNarudzbenice(idNarudzbenice);
+                frmStavkeNarudzbenice forma = new frmStavkeNarudzbenice(int.Parse(dataGridView1.CurrentRow.Cells[0].Value.ToString()));
                 forma.Show();
             }
             else
             {
                 MessageBox.Show("Niste odabrali narudzbenicu!");
+            }
+        }
+
+        private void uiActionObrisi_Click(object sender, EventArgs e)
+        {
+            
+            bool zastavica = false;
+            if (dataGridView1.CurrentRow != null)
+            {
+                int varijabla = int.Parse(dataGridView1.CurrentRow.Cells[0].Value.ToString());
+                using (var db = new MSDomEntities())
+                {
+                    var listaStavkiNarudzbenice = from nar in db.narudzbenicas
+                                                  join stav in db.stavkeNarudzbenices
+                                                  on nar.id equals stav.narudzbenicaId
+                                                  where nar.id == varijabla
+                                            select new { stav.id };
+
+                    if (listaStavkiNarudzbenice.ToArray().Length>0)
+                    {
+                        zastavica = true;
+                    }
+                    
+                }
+                if (!zastavica)
+                {
+                    int oznaceni = int.Parse(dataGridView1.CurrentRow.Cells[0].Value.ToString());
+                    using (var db = new MSDomEntities())
+                    {
+                        var ids = oznaceni;
+                        var sql = "Delete from [narudzbenica] WHERE id = {0}";
+                        db.Database.ExecuteSqlCommand(sql, ids);
+
+                    }
+                    DohvatiNarudzbenice();
+                }
+                else
+                {
+                    MessageBox.Show("Morate prvo obrisati stavke narudžbenice!");
+                }
+                
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            frmNarudzbenice forma = new frmNarudzbenice();
+            forma.Show();
+        }
+
+        private void uiActionIspis_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.CurrentRow != null)
+            {
+
+                List<IspisPdf.StavkeNarudzbenice> listalijekova;
+                int varijabla = int.Parse(dataGridView1.CurrentRow.Cells[0].Value.ToString());
+                using (var db = new MSDomEntities())
+                {
+                    var listaStavkiNarudzbenice = from nar in db.narudzbenicas
+                                                  join stav in db.stavkeNarudzbenices
+                                                  on nar.id equals stav.narudzbenicaId
+                                                  join lij in db.lijeks
+                                                  on stav.lijekId equals lij.id
+                                                  where nar.id == varijabla
+                                                  select new IspisPdf.StavkeNarudzbenice { Kolicina = stav.kolicina, Naziv = lij.naziv };
+
+                    listalijekova = listaStavkiNarudzbenice.ToList();
+                }
+
+                if (Ispis.IspisNarudzbenice(varijabla, listalijekova) == true)
+                {
+                    Ispis.IspisNarudzbenice(varijabla, listalijekova);
+                }
+                else
+                {
+                    MessageBox.Show("PDF je već otvoren");
+                }
             }
         }
     }
